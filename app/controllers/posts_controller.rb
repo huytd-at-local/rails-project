825ba51a872477cc4c40912105a24bae07b9acc0
huy-at-local <huytd.media@gmail.com>
2020-05-31 11:34:25 +0700

    post creating, editing, remove function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,40 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    if @post.save
+      flash[:notice] = "create success"
+      redirect_to("/posts/#{@post.id}")
+    else
+      render("posts/new")
+    end
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "edit success"
+      redirect_to("/posts/#{@post.id}")
+    else
+      render("posts/edit")
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "delete success"
+    redirect_to("/posts/index")
+  end
 end
