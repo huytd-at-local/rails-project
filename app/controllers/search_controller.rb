c235ae03b5487cfba6edfede8b86c9e67b2551de
Tu Ngo <tu.ngo@ntq-solution.com.vn>
2020-06-18 17:09:47 +0700

    search function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,16 +6,9 @@ class SearchController < ApplicationController
   
   def search
     keyword = params[:keyword]
-    url = URI.encode("https://www.googleapis.com/books/v1/volumes?q=#{keyword}&country=VN")
+    url = URI.encode("https://www.googleapis.com/books/v1/volumes?q=#{keyword}")
     results = HTTParty.get(url)
-      if results["items"]
-        @title = results["items"][0]["volumeInfo"]["title"]
-        @authors = results["items"][0]["volumeInfo"]["authors"][0]
-        @description = results["items"][0]["volumeInfo"]["description"]
-        @image = results["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
-      else
-        @test = "test"
-      end
+    @items = results["items"]
   end
 
 end
