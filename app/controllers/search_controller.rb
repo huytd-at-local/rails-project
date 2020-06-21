400e99fff1f847c2eb1544978cc86c22d2aa8889
huy-at-local <huytd.media@gmail.com>
2020-06-21 20:40:12 +0700

    search logic

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,8 @@ require 'httparty'
 class SearchController < ApplicationController
   
   def search
-    keyword = params[:keyword]
-    url = URI.encode("https://www.googleapis.com/books/v1/volumes?q=#{keyword}")
+    @keyword = params[:keyword]
+    url = URI.encode("https://www.googleapis.com/books/v1/volumes?q=#{@keyword}")
     results = HTTParty.get(url)
     @items = results["items"]
   end
